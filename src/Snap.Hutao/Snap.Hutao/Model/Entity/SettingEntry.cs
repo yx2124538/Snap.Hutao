323884a401593c7997d200bb1c79d262291773f3
DismissedLight <1686188646@qq.com>
2025-11-14 17:16:37 +0800

    [skip ci] code style

--- src/Snap.Hutao/Snap.Hutao/Model/Entity/SettingEntry.cs
+++ src/Snap.Hutao/Snap.Hutao/Model/Entity/SettingEntry.cs
@@ -1,7 +1,6 @@
 // Copyright (c) DGP Studio. All rights reserved.
 // Licensed under the MIT license.
 
-using Snap.Hutao.Core.Setting;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
